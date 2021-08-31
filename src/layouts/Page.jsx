d04984ecfabf6af4b8a5362f91a41736278fcae4
Dariusz Okonski <darek200180@gmail.com>
2021-09-01 00:50:45 +0100

    add specific Pages

--- src/layouts/Page.jsx
+++ src/layouts/Page.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { Route, Switch } from 'react-router-dom'
+import AdminPage from '../pages/AdminPage';
+import ContactPage from '../pages/ContactPage';
+import ErrorPage from '../pages/ErrorPage';
+import HomePage from '../pages/HomePage';
+import ProductPage from '../pages/ProductPage';
 
 const Page = () => {
     return ( 
-        <div>Page</div>
+        <div>
+            <Switch>
+                <Route exact path="/" component={HomePage} />                
+                <Route path="/products" component={ProductPage} />
+                <Route path="/contact" component={ContactPage} />
+                <Route path="/admin" component={AdminPage} />
+                <Route component={ErrorPage} />
+            </Switch>
+        </div>
      );
 }
  

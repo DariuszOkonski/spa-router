5947ed3c823f0ddcf37b5f5dbc44dd1360a0150b
Dariusz Okonski <darek200180@gmail.com>
2021-08-31 23:19:18 +0100

    add basic components

---
+++ src/layouts/App.js
@@ -0,0 +1,33 @@
+import { BrowserRouter as Router } from 'react-router-dom';
+import '../styles/App.css';
+import Footer from './Footer';
+import Header from './Header';
+import Navigation from './Navigation';
+import Page from './Page';
+
+function App() {
+  return (
+    <Router>      
+      <div className="App">
+        <header>
+          <Header/>
+        </header>
+        
+        <main>
+          <aside>
+            <Navigation />
+          </aside>
+          <section className="page">
+            <Page/>
+          </section>
+        </main>
+
+        <footer>
+          <Footer/>
+        </footer>
+      </div>
+    </Router>
+  );
+}
+
+export default App;

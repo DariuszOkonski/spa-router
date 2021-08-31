556b9ea8787f73223d33fa9446a891f772f7f17e
Dariusz Okonski <darek200180@gmail.com>
2021-09-01 00:28:41 +0100

    build displaying different images regards url

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import '../styles/Header.css';
+import img1 from '../images/header1.jpg';
+import img2 from '../images/header2.jpg';
+import img3 from '../images/header3.jpg';
+import { Route } from 'react-router-dom';
 
 const Header = () => {
     return ( 
-        <div>Header</div>
+        <>
+            <Route exact path="/" render={() => (
+                <img src={img1} alt="city" />
+            )} />
+
+            <Route path="/products" render={() => (
+                <img src={img2} alt="city" />
+            )} />
+
+            <Route path="/contact" render={() => (
+                <img src={img3} alt="city" />
+            )} />
+
+            <Route path="/admin" render={() => (
+                <img src={img2} alt="city" />
+            )} />
+        </>
      );
 }
  

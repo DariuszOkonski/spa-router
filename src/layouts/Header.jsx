24f7ef66758b630d5c7c0ae6fc7c3aa264c9f263
Dariusz Okonski <darek200180@gmail.com>
2021-09-01 00:38:28 +0100

    add default route for header

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -3,26 +3,32 @@ import '../styles/Header.css';
 import img1 from '../images/header1.jpg';
 import img2 from '../images/header2.jpg';
 import img3 from '../images/header3.jpg';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 const Header = () => {
     return ( 
         <>
-            <Route exact path="/" render={() => (
-                <img src={img1} alt="city" />
-            )} />
+            <Switch>
+                <Route exact path="/" render={() => (
+                    <img src={img1} alt="city" />
+                    )} />
 
-            <Route path="/products" render={() => (
-                <img src={img2} alt="city" />
-            )} />
+                <Route path="/products" render={() => (
+                    <img src={img2} alt="city" />
+                    )} />
 
-            <Route path="/contact" render={() => (
-                <img src={img3} alt="city" />
-            )} />
+                <Route path="/contact" render={() => (
+                    <img src={img3} alt="city" />
+                    )} />
 
-            <Route path="/admin" render={() => (
-                <img src={img2} alt="city" />
-            )} />
+                <Route path="/admin" render={() => (
+                    <img src={img2} alt="city" />
+                    )} />
+                
+                <Route render={() => (
+                    <img src={img1} alt="city" />
+                )} />
+            </Switch>
         </>
      );
 }

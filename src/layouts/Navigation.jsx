5c5c3e0af44d3c4d33bb50170c07ef5ee874c1af
Dariusz Okonski <darek200180@gmail.com>
2021-09-01 00:01:31 +0100

    style left menu

--- src/layouts/Navigation.jsx
+++ src/layouts/Navigation.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+import '../styles/Navigation.css';
+
+const list = [
+    {name: 'start', path: '/', exact: true},
+    {name: 'produkty', path: '/products', exact: false},
+    {name: 'kontakt', path: '/contact', exact: false},
+    {name: 'panel admina', path: '/admin', exact: false},
+]
 
 const Navigation = () => {
+    const menu = list.map(item => 
+        <li key={item.name}>
+            <NavLink exact={item.exact} to={item.path}>{item.name}</NavLink>
+        </li>
+    )
+
     return ( 
-        <div>Navigation</div>
-     );
+        <nav className="main">
+            <ul>
+                {menu}
+            </ul>
+        </nav>
+    );
 }
  
 export default Navigation;

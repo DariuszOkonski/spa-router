9a906f46a3b03e8cc24e3fbbe42a793795d4a0ef
Dariusz Okonski <darek200180@gmail.com>
2021-09-01 23:16:34 +0100

    build Article Component

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,35 @@
 import React from 'react'
+import Article from '../components/Article';
+
+const articles = [
+    {
+        id: 1,
+        title: "Czym jest teoria strun",
+        author: "Jan Nowak",
+        text: "111Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus voluptatum eum eaque quisquam sunt blanditiis veniam sed repellendus, delectus omnis, laboriosam impedit deserunt eius, praesentium voluptatem illum quae soluta! Culpa."
+    },
+    {
+        id: 2,
+        title: "Czym jest paradoks fermiego?",
+        author: "Ania Kwiatkowska",
+        text: "222Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus voluptatum eum eaque quisquam sunt blanditiis veniam sed repellendus, delectus omnis, laboriosam impedit deserunt eius, praesentium voluptatem illum quae soluta! Culpa."
+    },
+    {
+        id: 3,
+        title: "Ciemna materia i ciemna energia?",
+        author: "Jan Kowalski",
+        text: "333Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus voluptatum eum eaque quisquam sunt blanditiis veniam sed repellendus, delectus omnis, laboriosam impedit deserunt eius, praesentium voluptatem illum quae soluta! Culpa."
+    }
+]
 
 const HomePage = () => {
+    const artList = articles.map(article => (
+        <Article key={article.id} {...article} />
+    ))
+
     return (
-        <div>
-            <h1>Strona Startowa</h1>            
+        <div className="home">
+            {artList}
         </div>  
     );
 }

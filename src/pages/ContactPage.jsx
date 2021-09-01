ef3c8efcd8a7166c3c58940e08bcba9f1e75977b
Dariusz Okonski <darek200180@gmail.com>
2021-09-01 23:43:05 +0100

    add ContactPage logic

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,11 +1,46 @@
-import React from 'react'
+import React, { Component } from 'react';
+import { Prompt } from 'react-router-dom';
+import '../styles/ContactPage.css';
 
-const ContactPage = () => {
-    return (  
-        <div>
-            <h1>ContactPage</h1>
-        </div>
-    );
+class ContactPage extends Component {
+    state = {
+        value: ""
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        
+        this.setState({
+            value: ''
+        })
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            value: e.target.value
+        })
+    }
+    
+    render() { 
+        return ( 
+            <div className="contact">
+                <form onSubmit={this.handleSubmit}>
+                    <h3>Napisz do nas</h3>
+                    <textarea 
+                        value={this.state.value}
+                        onChange={this.handleChange}
+                        placeholder="Wpisz wiadomość..."
+                    ></textarea>
+                    
+                    <button>Wyślij</button>
+                </form>
+                <Prompt 
+                    when={this.state.value.length > 0}
+                    message="Masz niewypełniony formularz. Czy napewno chcesz opuścić tę stronę?"
+                />
+            </div>
+         );
+    }
 }
  
 export default ContactPage;
